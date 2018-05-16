dbec326b135dd31185175414b2a83fb99156159b
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-05-16 14:59:46 -0500

    Completed increase and decrease functionality

--- src/counter.js
+++ src/counter.js
@@ -1,27 +1,35 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
-    // constructor() {
-    //     super();
-    //     //makes "this" work for increase button
-    //     this.increaseClick = this.increaseClick.bind(this);
-    //     //makes "this" work for decrease button
-    //     this.decreaseClick = this.decreaseClick.bind(this);
-    // }
+    constructor(props) {
+        super(props);
+        //Create state object
+        this.state = {num: 0}
+        //Make "this" in increase() point to the right place
+        // this.increase = this.increase.bind(this);
+        //Make "this" in decrease() point to the right place
+        // this.decrease = this.decrease.bind(this);
+    }
     increase(){
-        alert("You increased");
+        // alert("You increased");
+        this.setState({
+            num: this.state.num + 1
+        });
     };
 
     decrease() {
-        alert("You decreased");
+        // alert("You decreased");
+        this.setState({
+            num: this.state.num - 1
+        });
     };
 
     render(){
         return (
             <div>
-                <h2>0</h2>
-                <button onClick={this.increase}>Increase</button>
-                <button onClick={this.decrease}>Decrease</button>
+                <h2>{this.state.num}</h2>
+                <button onClick={this.increase.bind(this)}>Increase</button>
+                <button onClick={this.decrease.bind(this)}>Decrease</button>
             </div>
         );
     }
