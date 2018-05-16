c96046e678a0ebda6d898f603c9c15d15f5823ae
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-05-16 11:55:51 -0500

    Add basic click listener to buttons

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
+    // constructor() {
+    //     super();
+    //     //makes "this" work for increase button
+    //     this.increaseClick = this.increaseClick.bind(this);
+    //     //makes "this" work for decrease button
+    //     this.decreaseClick = this.decreaseClick.bind(this);
+    // }
+    increase(){
+        alert("You increased");
+    };
+
+    decrease() {
+        alert("You decreased");
+    };
+
     render(){
         return (
             <div>
                 <h2>0</h2>
-                <button>Increase</button>
-                <button>Decrease</button>
+                <button onClick={this.increase}>Increase</button>
+                <button onClick={this.decrease}>Decrease</button>
             </div>
         );
     }
